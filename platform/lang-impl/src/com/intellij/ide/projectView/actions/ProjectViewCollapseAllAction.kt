f6ec1ff6d77e106abe87a07c89dd30a83f354a07
avromi-s <101535424+avro3@users.noreply.github.com>
2024-12-10 17:00:39 -0500

    add Collapse Selected button

---
+++ platform/lang-impl/src/com/intellij/ide/projectView/actions/ProjectViewCollapseAllAction.kt
@@ -0,0 +1,36 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.ide.projectView.actions
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys.TREE_EXPANDER
+import com.intellij.openapi.actionSystem.remoting.ActionRemoteBehaviorSpecification
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.util.registry.Registry
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.ApiStatus.Experimental
+
+@ApiStatus.Internal
+@Experimental
+class ProjectViewCollapseAllAction : DumbAwareAction(), ActionRemoteBehaviorSpecification.Frontend {
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val expander = getTreeExpander(e) ?: return
+    if (expander.canCollapse()) {
+      expander.collapseAll()
+    }
+  }
+
+  override fun update(event: AnActionEvent) {
+    val expander = getTreeExpander(event)
+    event.presentation.isVisible = false
+    event.presentation.isEnabled = expander != null && expander.canCollapse() && !expander.isCollapseAllEnabled
+                                   Registry.`is`("ide.project.view.replace.collapse.all.with.collapse.recursively", true)
+  }
+
+  private fun getTreeExpander(e: AnActionEvent) = e.getData(TREE_EXPANDER)
+
+  override fun getActionUpdateThread(): ActionUpdateThread {
+    return ActionUpdateThread.EDT
+  }
+}
